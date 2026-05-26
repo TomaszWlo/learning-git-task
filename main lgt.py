001610d3f45fef7545de9c163814f75fc747cb50
Tomasz Włodarczak <tomyslav82@gmail.com>
2026-05-26 16:13:02 +0200

    First change

--- main lgt.py
+++ main lgt.py
@@ -1,15 +1,3 @@
-zakupy = {
-    'pieczywko' : ['chleb', 'bulki', 'paczek'],
-    'warzywko': ['marchew', 'seler', 'rukola']
-}
+print('dodaje drugi commit zgodnie z poleceniem  z cwiczenia')
 
-
-suma = len(zakupy['pieczywko']) + len(zakupy['warzywko'])
-
-for sklep, towar in zakupy.items():
-        print(f'Idę do sklepu {sklep.upper()} i kupuje tam {[t.upper() for t in towar]}')
-        
-print(f'W sumie kupuje {suma} produktow')
-
-print('Skecz Hiszpańska inkwizycja to najlepszy skecz Monty Pythona')
 
