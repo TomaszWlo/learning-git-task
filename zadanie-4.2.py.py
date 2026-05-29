9f4479bc52b955ad37ef96cf185fa2fd85615541
Tomasz Włodarczak <tomyslav82@gmail.com>
2026-05-29 20:00:33 +0200

    Added docString

--- zadanie-4.2.py.py
+++ zadanie-4.2.py.py
@@ -1,4 +1,10 @@
 def word_check(word):
+
+    '''
+    Funkcja sprawdza czy podany argument string jest palindromem
+    czyli slowem które czyta sie tak samo od przodu i od tylu
+    '''
+
     if word == word[::-1]:
         print('Czy argument jest palindromem : Prawda')
     else:
