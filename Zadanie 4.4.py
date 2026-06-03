4fcc7ac8581b451ffdd65e02f8d0906216528f35
Tomasz Włodarczak <tomyslav82@gmail.com>
2026-06-03 08:15:19 +0200

    init zadanie 4.4

---
+++ Zadanie 4.4.py
@@ -0,0 +1,39 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+
+operacja = int(input('Podaj działanie, poslugujac sie odpowiednia liczba: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie : '))
+
+
+if operacja == 1:
+    ilosc_liczb = int(input('Ile liczb chcesz dodać ? : '))
+    suma = 0
+
+    for i in range(ilosc_liczb):
+        liczba = float(input(f'Podaj liczbe nr: {i + 1}:'))
+        suma += liczba   
+    print(f'Wynik to {suma}')
+
+elif operacja == 2:
+    nr1 = float(input('Podaj liczbe nr 1: '))
+    nr2 = float(input('Podaj liczbe nr 2: '))
+
+    logging.info(f'Odejmuje {nr1} i {nr2}')
+    odejmowanie = float(nr1) - float(nr2)
+    print(f'Wynik to {odejmowanie}')
+
+elif operacja == 3:
+    ilosc_mnoznikow = int(input('Ile liczb chcesz pomnożyć ? : '))
+    iloczyn = 1
+    
+    for i in range(ilosc_mnoznikow):
+        mnoznik = float(input(f'Podaj liczbe nr {i +1} :'))
+        iloczyn *= mnoznik
+    print(f'Wynik to {iloczyn}')
+
+elif operacja == 4:
+    nr1 = float(input('Podaj liczbe nr 1: '))
+    nr2 = float(input('Podaj liczbe nr 2: '))
+
+    logging.info(f'Dzielę {nr1} i {nr2}')
+    dzielenie = float(nr1) / float(nr2)
+    print(f'Wynik to {dzielenie}')
